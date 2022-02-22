9b470f20233da39421b771b7a3583ee3326703de
Pete982 <84607563+Pete982@users.noreply.github.com>
2022-02-21 21:45:08 -0800

    Update and rename Chatting.js to Third_Page.js

--- Third_Page.js
+++ Third_Page.js
@@ -15,12 +15,12 @@ firebase.initializeApp(firebaseConfig);
   user_name = localStorage.getItem("Username");
   document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
 
-  function addroom() {
+  function addRoom() {
          room_name = document.getElementById("room_name").value;
 
         localStorage.setItem("Roomname",room_name);
     
-        window.location = "Chatting.html";
+        window.location = "Third_Page.html";
   }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
